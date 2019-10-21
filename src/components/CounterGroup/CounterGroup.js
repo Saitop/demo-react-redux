8f3451178c3dea7c2b12c77d9812ef26a9120ca5
Hanxian Lin <saitoplam@hotmail.com>
2019-10-21 16:05:32 +0800

    generate counters by input value

--- src/components/CounterGroup/CounterGroup.js
+++ src/components/CounterGroup/CounterGroup.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import Counter from "../Counter/Counter";
+import './CounterGroup.css'
 
 class CounterGroup extends Component {
   constructor(props) {
     super(props);
+    // this.handleInputChange.bind(this);
+    this.state = { counterCounts: this.props.defaultCounts, inputValue: this.props.defaultCounts}
   }
 
+  handleInputChange = (event) => {
+    this.setState({inputValue: event.target.value})
+    console.log("inputValue = ", this.state.inputValue)
+  };
+  regernateCounters = () =>  {
+    this.setState({counterCounts: this.state.inputValue})
+  };
   render() {
     let counters = [];
-    for (let count = 0; count < this.props.defaultCounts; count ++ ) {
+    for (let count = 0; count < this.state.counterCounts; count ++ ) {
       counters.push(
         <Counter />
       )
     }
     return (
-      <div>
-        <input type="text"/>
-        <button>Regenrate counters</button>
-        <span>Sum: 0</span>
-        {counters}
+      <div className="counter-group">
+        <div className="regenerate">
+          <input type="text" value={this.state.inputValue} onChange={this.handleInputChange}/>
+          <button onClick={this.regernateCounters}>Regenrate counters</button>
+          <span>Sum: 0</span>
+        </div>
+        <div>
+          {counters}
+        </div>
       </div>
     );
   }
